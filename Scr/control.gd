cde36af362ab5fc33c22f971718c42301935c7b3
tozeleal <tozevleal@gmail.com>
2023-05-28 01:02:55 +0100

    Updated git ignore, changed files folder

---
+++ Scr/control.gd
@@ -0,0 +1,13 @@
+extends Control
+var teclas_tex1 = preload("res://assets/sprites/keyboard/arrows.png")
+var teclas_tex2 = preload("res://assets/sprites/keyboard/awsd.png")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	while(true)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
---
+++ Scr/main_menu.gd
@@ -0,0 +1,29 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$VBoxContainer/StartGameButton.grab_focus()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+
+func _on_start_game_button_pressed():
+	get_tree().change_scene_to_file("res://Src/game.tscn")
+
+
+func _on_controls_button_pressed():
+	get_tree().change_scene_to_file("res://Src/control.tscn")
+
+
+func _on_credits_button_pressed():
+	var credits = load("res://Src/credits.tscn")
+	get_tree().current_scene.add_child(credits)
+
+
+func _on_exit_button_pressed():
+	get_tree().quit()
